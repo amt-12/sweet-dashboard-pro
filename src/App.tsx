17f05ffe85b459bdeb2394a0fae9693da2ff48ae
amanbadyal7214 <amanbadyal94@gmail.com>
2026-03-17 14:36:58 +0530

    admin panal

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Lazy-loaded pages and admin layout
-const Home = lazy(() => import("./pages/Home"));
-const Menu = lazy(() => import("./pages/Menu"));
-const About = lazy(() => import("./pages/About"));
-const Gallery = lazy(() => import("./pages/Gallery"));
-const Contact = lazy(() => import("./pages/Contact"));
-const ProductDetails = lazy(() => import("./pages/ProductDetails"));
-const CustomizeOrder = lazy(() => import("./pages/CustomizeOrder"));
-const Login = lazy(() => import("./pages/Login"));
+
 
 const AdminLayout = lazy(() => import("./components/AdminLayout"));
 const Index = lazy(() => import("./pages/Index"));
@@ -49,14 +42,6 @@ const App = () => (
           }
         >
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/menu" element={<Menu />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/gallery" element={<Gallery />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/product/:id" element={<ProductDetails />} />
-            <Route path="/customize-order" element={<CustomizeOrder />} />
-            <Route path="/login" element={<Login />} />
             <Route path="/admin" element={<AdminLayout />}>
               <Route index element={<Index />} />
               <Route path="orders" element={<Orders />} />
